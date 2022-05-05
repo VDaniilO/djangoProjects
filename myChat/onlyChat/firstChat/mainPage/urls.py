aa738fe284086db01d4ca0cf7a6c8fb5c6070724
Vasiliev Daniil <vasiliev.daniil.o@gmail.com>
2022-05-05 21:45:39 +0300

    Create template for experiments and test work

--- myChat/onlyChat/firstChat/mainPage/urls.py
+++ myChat/onlyChat/firstChat/mainPage/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', adminUsr),
+    path('expPage', adminUsr),
 ]
--- myChat/onlyChat/firstChat/mainPage/views.py
+++ myChat/onlyChat/firstChat/mainPage/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
+from .models import logInf, Regions
 # Create your views here.
 
 def adminUsr(request):
-    return HttpResponse('Hello')
+    usr = logInf.objects.all()#requst for get all inf usr in my db
+    res = '<h1>List users</h1>'
+
+    context = {
+        'title': 'List users',
+        'usersInf': usr,
+    }
+
+    return render(request, template_name = 'mainPage/expPage.html', context = context)
